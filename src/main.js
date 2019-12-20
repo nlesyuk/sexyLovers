c508e0ae5e4cd8572a89d50858996defbf64c342
Nazar Lesyuk-w <pronaza@gmail.com>
2019-12-20 10:43:49 +0200

    vue-meta: fixed - DONE

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Meta from 'vue-meta'
+import VueMeta from 'vue-meta'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMeta);
 
 new Vue({
   render: h => h(App),
