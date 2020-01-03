b002fd62bb97a1498c6c44bbe68a34c710a7a384
Nazar Lesyuk-w <pronaza@gmail.com>
2020-01-03 12:18:31 +0200

    change title

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import VueMeta from 'vue-meta'
 import VueMask from 'v-mask'
 import PhoneMaskInput from  "vue-phone-mask-input" // use as component <phone-mask-input/>
 import 'animate.css/animate.min.css'
-// title: 'Sexy lovers - Ищите интим без обязательств? Вы по адресу!',
 
 Vue.config.productionTip = false
 
@@ -16,14 +15,15 @@ new Vue({
     htmlAttrs: {
       lang: 'ru'
     },
-    title: 'title',
+    title: 'Sexy lovers - Ищите интим без обязательств? Вы по адресу!',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme-color', content: '#e73939' },
     ],
     link: [
-      { rel: 'icon', href: '../public/favicon.ico', type: "image/x-icon"}
+      // { rel: 'icon', href: '../public/favicon.ico', type: "image/x-icon"}
+      { rel: 'icon', href: '../public/icon.png', type: "image/x-icon"}
     ]
   },
   render: h => h(App)
